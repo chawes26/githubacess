29c2f52491bbf1387926d22c60622b669e4b4785
chawes26 <chase.hawes95@gmail.com>
2025-04-30 18:58:41 -0700

    BatchRandomization

---
+++ BatchRandomization.R
@@ -0,0 +1,23 @@
+rm(list=ls())
+library("experDesign")
+my_data <- read.csv("Filename.csv")
+head(my_data)
+check_data(my_data)
+set.seed(87732135)
+index <- design(pheno = my_data, size_subset = 21, omit = NULL, iterations = 100)
+index
+str(index)
+class(index)
+attributes(index)
+dim(index)
+length(index)
+my_array <- array(unlist(index), dim = c(21))
+print(my_array)
+my_lists <- list(index)
+my_lists
+flattened_vector <- unlist(my_lists)
+flattened_vector
+my_array <- array(flattened_vector, dim = c(21, 7))
+print(my_array)
+my_df <- as.data.frame(my_array)
+write.csv(my_df, "my_array.csv", row.names = FALSE)
